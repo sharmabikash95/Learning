7d9bfe0d3cfe5dc2e34a8d973055a07972363a95
Bikash Sharma <sharmabikas95@gmail.com>
2023-08-12 09:58:41 +1000

    this is my next changes

--- Apple_app/Apple_app/Program.cs
+++ Apple_app/Apple_app/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World , this is my first day");
+            Console.WriteLine("Hello, World , this is my new change");
         }
     }
 }
